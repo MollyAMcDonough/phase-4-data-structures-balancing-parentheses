2629054b75c2d4aa781635c80f9e4efa3d5ddcc1
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-04-12 22:27:42 -0400

    commit tree c9adb48482e9a37bc430cdbbc349bd4ca9e56a60

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,18 @@
 require_relative './stack'
 
 # your code here
+def balancing_parentheses(str)
+    stack = Stack.new
+    chars = str.split("")
+    closing = 0
+    chars.each do |c|
+        if c == '('
+            stack.push(c)
+        elsif c == ')' && stack.size == 0
+            closing += 1
+        else
+            stack.pop
+        end
+    end
+    closing + stack.size
+end
